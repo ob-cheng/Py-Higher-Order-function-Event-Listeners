68909af8eb12f044a1d96d83cf5a599f8b994260
ob-cheng <obadiahcheng@icloud.com>
2025-07-26 13:49:14 +0200

    Add project setup files and implement basic turtle race app
    Introduce IntelliJ project configuration files and initial scripts (`main.py` and `turtle_race.py`) for the turtle race application. The `turtle_race.py` script includes functionality for user input, turtle setup, random movements, and determining the race outcome. This serves as a foundation for further development.

--- turtle_race.py
+++ turtle_race.py
@@ -1,7 +1,46 @@
 from turtle import Turtle, Screen
+import random
 
 screen = Screen()
 screen.setup(width=500, height=400)
-user_input = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+user_input = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ").lower()
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = []
+game_on = False
 
+# tim = Turtle(shape="turtle")
+# tim.penup()
+# tim.goto(-230, -100)
+
+# Create six turtles
+initial_y = -100
+ini_color = 0
+for item in colors:
+    item = Turtle(shape='turtle')
+    item.color(colors[ini_color])
+    ini_color += 1
+    item.penup()
+    item.goto(x = -230, y = initial_y)
+    initial_y += 33.3
+    all_turtles.append(item)
+
+print(all_turtles)
+if user_input:
+    game_on = True
+
+while game_on:
+    for obj in all_turtles:
+        move_distance = random.randint(0,10)
+        obj.forward(move_distance)
+        # print(obj.xcor())
+
+        if obj.xcor() >= 230:
+            game_on = False
+            winner = obj.pencolor()
+
+if user_input == winner:
+    print("You won")
+else:
+    print("you lose")
+
+# screen.exitonclick()
